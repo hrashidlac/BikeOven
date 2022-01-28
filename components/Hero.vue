8935bf4dfec37dbb331bd50e1f98ab04bbe7136e
Harris Rashid <hrashid@clippers.com>
2022-01-28 13:15:21 -0800

    Hero

---
+++ components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hero relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
+    <img class="slogan" src="@/assets/img/slogan.png">
+  </div>
+</template>
+<style scoped>
+.hero{
+  height:750px;
+  width:100%;
+  background:transparent url(@/assets/img/hero.jpg) no-repeat center center;
+  background-size:cover;
+}
+.slogan{
+  max-width:300px;
+}
+</style>
